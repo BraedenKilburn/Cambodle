c1f43a27b660b1d6d2517f3b1a12c7880932650c
Braeden Kilburn <braeden.kilburn@vivint.com>
2024-04-30 10:54:29 -0700

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,10 +10,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  test: {
-    environment: 'jsdom',
-    root: fileURLToPath(new URL('./', import.meta.url)),
   },
   server: {
     port: 3000
